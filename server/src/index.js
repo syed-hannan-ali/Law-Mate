2ca09f071b2f4f4f0dc60094b19b61018cdea189
syed-hannan-ali <xyed.hannan75@outlook.com>
2025-07-23 16:19:51 +0500

    tested some end points

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 require("module-alias/register");
 require("dotenv").config();
 
+const User = require("@models/user.js");
+
 const express = require("express");
 
 const { connectDB } = require("@config/db.js");
@@ -12,8 +14,24 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_req, res) => {
-    res.send("🚀 Server is running");
+app.get("/", async (_req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (e) {
+        console.error("Error fetching user:", e);
+        res.send({ e });
+    }
+});
+
+app.post("/api/test", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (e) {
+        console.error("Error creating user:", e);
+        res.send({ error: e });
+    }
 });
 
 app.listen(PORT, () => {
